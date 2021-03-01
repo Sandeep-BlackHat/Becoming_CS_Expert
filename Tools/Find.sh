ed9f93bb386cb6d0477e6384cc536129c14e123f
Ryuzaki Lawliet <55851630+Sandeep-BlackHat@users.noreply.github.com>
2021-03-01 22:00:27 +0530

    Create Find.sh

---
+++ Tools/Find.sh
@@ -0,0 +1,9 @@
+<h1>Find Commands for Linux</h1>
+
+kali@kali:~$ whatis find
+kali@kali:~$ man find
+kali@kali:~$ sudo find / -type f -name "proxychains.conf"
+kali@kali:~$ sudo find / -type f -iname "proxychains.conf" #for disabling case sensitive wordings
+kali@kali:~$ sudo find / -type f -name "*.conf" #all files with <.conf> extension will be displayed
+kali@kali:~$ sudo find / -type f -perm 400 #all files with perimission defined will be displayed
+kali@kali:~$ sudo find / -type f -name "proxychains.conf" -size +1K #basically sizes like 1024 K have to be written like 1024c in the command it's complusory
