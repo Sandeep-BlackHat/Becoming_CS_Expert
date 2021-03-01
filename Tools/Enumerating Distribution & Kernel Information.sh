07cf28a86c950dc035a45c81b296fef137d066ab
Ryuzaki Lawliet <55851630+Sandeep-BlackHat@users.noreply.github.com>
2021-03-01 21:53:04 +0530

    Create Enumerating Distribution & Kernel Information.sh

---
+++ Tools/Enumerating Distribution & Kernel Information.sh
@@ -0,0 +1,17 @@
+<h1>Commands</h1>
+
+kali@kali:~$ whoami
+kali@kali:~$ hostname
+kali@kali:~$ sudo vim /etc/hostname
+kali@kali:~$ id
+kali@kali:~$ groups <name>
+kali@kali:~$ lsb_release -a/-s/-r/-p
+kali@kali:~$ cat /etc/issue
+kali@kali:~$ cat /etc/os-release
+kali@kali:~$ cat /etc/release #for CentOS, Arch OS
+kali@kali:~$ cat /etc/*release
+kali@kali:~$ cat /etc/redhat-release #for RedHat OS
+kali@kali:~$ lscpu
+kali@kali:~$ whatis uname
+kali@kali:~$ uname -a/-s/-r/-p/-i/-o
+kali@kali:~$ who
